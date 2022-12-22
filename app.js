0d0fa4190c63119b4ded8190a51c8311db4619fb
Ashish-1104 <ashishkokare5@gmail.com>
2022-12-23 03:04:10 +0530

    Merging and styling

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 let previous = document.querySelector(".previous");
 let next = document.querySelector(".next");
+let first_page = document.querySelector(".first_page");
+let last_page = document.querySelector(".last_page");
 let coinGekuUrl = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${rows}&page=1&sparkline=false";
 let usd, eur, inr;
 let page = 1;
-let rows=15;
+let rows=10;
 let add = document.querySelector(".coinTable");
 previous.addEventListener("click",async () => {
     if (page > 1) {
@@ -27,6 +29,19 @@ next.addEventListener("click", async() => {
     }
 });
 
+first_page.addEventListener("click",async()=>{
+    page=1;
+    for(var i=rows;i>0;i--)
+        add.deleteRow(i);
+    await getData();
+})
+
+last_page.addEventListener("click",async()=>{
+    page=10;
+    for(var i=rows;i>0;i--)
+        add.deleteRow(i);
+    await getData();
+})
 function genTable() {
 
     for (var i = 0; i < rows; i++) {
@@ -52,6 +67,11 @@ function genTable() {
 
 let getData = async () => {
 
+    let crypto=document.querySelector(".coinTable");
+    let load=document.createElement("img")
+    load.src="images/loading.gif";
+    load.classList.add("loadingGif");
+    crypto.appendChild(load);
     await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${rows}&page=${page}&sparkline=false`)
         .then((res) => {
             return res.json()
@@ -77,6 +97,7 @@ let getData = async () => {
         .then((data) => {
             inr = data;
         })
+        load.remove();
 
     genTable();
 }
